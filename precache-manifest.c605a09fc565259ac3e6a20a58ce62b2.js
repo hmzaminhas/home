fb468fa3eadd0ce081260ac182192b2a21be2311
Hamza Minhas <minhas437@gmail.com>
2020-11-14 21:04:59 +0500

    Updates

---
+++ precache-manifest.c605a09fc565259ac3e6a20a58ce62b2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba03ab98d7c13ed7edd2df1d88d29c75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b6464f7719fe6584baa",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "57869e7269f237aa8835",
+    "url": "/static/js/2.c85af4d1.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.c85af4d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b6464f7719fe6584baa",
+    "url": "/static/js/main.6417ef7d.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "6e69d29eb67f808b012d95bab11e6a08",
+    "url": "/static/media/DSC_2977.6e69d29e.jpg"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  }
+]);
